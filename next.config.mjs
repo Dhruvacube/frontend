e1d21fbcbced9092992d77627d8c182b0054e91e
Dhruva Shaw <me@dhruvashaw.me>
2024-03-29 12:43:53 +0530

    export temp fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    images: {
-      domains: ["cdn.jsdelivr.net", "dummyimage.com", "i.ytimg.com", "yt3.ggpht.com"],
-      unoptimized: false,
-    },
-    swcMinify: true,
+  images: {
+    domains: ["cdn.jsdelivr.net", "dummyimage.com", "i.ytimg.com", "yt3.ggpht.com"],
+    unoptimized: false,
+  },
+  swcMinify: true,
+  distDir: "build",
+  output: "export",
 };
 
 export default nextConfig;
